40714d0ef93f4ee3a37a489849d56fa619b43b02
Symion Edwards <symion.edwards@gmail.com>
2020-03-24 10:07:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,7 @@ function accessElementInArray (array, index) {
   return array[index]
 }
 
-destructivelyRemoveElementFromBeginningOfArray (array) {
-  array.shift() ; {
-  return array}
+function destructivelyRemoveElementFromBeginningOfArray (array) {
+  array.shift() 
+  return array
 }
