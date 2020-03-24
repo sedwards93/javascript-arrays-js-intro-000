9f3fa951f7b6641736d1a3397ee6afd1dab92569
Symion Edwards <symion.edwards@gmail.com>
2020-03-24 10:22:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -37,5 +37,6 @@ function destructivelyRemoveElementFromEndOfArray (array) {
 }
 
 function removeElementFromEndOfArray (array) {
-  return array.slice (0, array.length- 1)
+  array.slice (0, array.length- 1);
+  return array
 }
